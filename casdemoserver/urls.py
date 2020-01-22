a9cc15a4b98b1d8d3be7872516f30082e08cf5dc
Ming <ming@djangocas.dev>
2020-01-22 15:47:10 -0800

    Disable /admin urls

--- casdemoserver/urls.py
+++ casdemoserver/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     url(r'^cas/', include('mama_cas.urls')),
 ]
